8924db6f9aa449eb77a545a43261f3b2d495dce4
guozhengshui <guozhengshui@xiaoniu.com>
2018-11-08 14:16:38 +0800

    新增功能:IOS企业签管理

---
+++ src/views/iosCompanySign/edit.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="border">
+    <el-dialog title="更新IOS企业签" :visible.sync="ifshow" @open="openDialog" :before-close="closeDialog">
+      <el-form :inline="true" :model="iosCompanySignForm" :rules="rules" ref="iosCompanySignForm" label-width="100px" class="demo-form-inline">
+        <el-row type="flex" justify="center">
+          <el-col :span="40">
+            <el-form-item label="手机系统">
+              <el-input v-model="iosCompanySignForm.osType" placeholder="手机系统"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="40">
+            <el-form-item label="下载链接key">
+              <el-input v-model="iosCompanySignForm.urlKey" placeholder="下载链接key"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="center">
+          <el-col :span="40">
+            <el-form-item label="下载链接">
+              <el-input v-model="iosCompanySignForm.url" placeholder="下载链接"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="40">
+            <el-form-item label="优先级">
+              <el-input-number v-model="iosCompanySignForm.priority" controls-position="right" :min="1" :max="999"></el-input-number>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="center">
+          <el-col :span="40">
+            <el-form-item label="应用号">
+              <el-select v-model="iosCompanySignForm.appNum" clearable placeholder="请选择">
+                <el-option v-for="item in globalConfig.appNames" :key="item.value" :label="item.label" :value="item.value"/>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="40" justify="center">
+            <el-form-item label="备注说明">
+              <el-input v-model="iosCompanySignForm.remark" placeholder="备注说明"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="center">
+          <el-col :span="40">
+            <el-form-item label="状态">
+              <el-switch style="margin: 10px;" v-model="iosCompanySignForm.status" :active-value="true" :inactive-value="false" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+            </el-form-item>
+          </el-col>
+          <el-col :span="40">
+            <el-form-item>
+              <el-input v-model="iosCompanySignForm.id" type="hidden"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="center">
+          <el-col :span="40">
+            <el-form-item>
+              <el-button @click="closeDialog">取消</el-button>
+              <el-button type="primary" @click="saveIosCompanySign">确定</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import debounce from 'throttle-debounce/debounce'
+import {clone} from '@/utils/common'
+export default {
+  props: {
+    'ifshow': Boolean,
+    'iosCompanySign': Object
+  },
+  data () {
+    return {
+      iosCompanySignForm: {},
+      rules: {}
+    }
+  },
+  methods: {
+    openDialog () {
+      this.iosCompanySignForm = clone(this.iosCompanySign)
+    },
+    closeDialog () {
+      this.$refs['iosCompanySignForm'].resetFields()
+      this.$emit('handleCloseDialog')
+    },
+    saveIosCompanySign: debounce(300, function () {
+      this.$refs['iosCompanySignForm'].validate((valid) => {
+        if (valid) {
+          this.$confirm('确认更新吗？')
+            .then(async () => {
+              try {
+                const res = await this.$http.put('/ios-company-sign', this.iosCompanySignForm)
+                if (res.code === '200') {
+                  this.$message.success('更新成功!')
+                  this.closeDialog()
+                } else {
+                  this.$message.error(res.message)
+                }
+              } catch (err) {
+                console.error(err)
+              }
+            })
+            .catch(() => {})
+        }
+      })
+    })
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
---
+++ src/views/iosCompanySign/list.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="border">
+    <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+      <el-form-item style="float: left">
+        <el-button type="primary" icon="el-icon-plus" @click="showAddFlag = true">新增</el-button>
+        <el-button type="danger" icon="el-icon-delete" @click="removeIosCompanySign">删除</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table ref="iosCompanySignTable" :data="tableData" border stripe highlight-current-row @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55">
+      </el-table-column>
+      <el-table-column prop="id" label="ID" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="osType" label="手机系统" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="urlKey" label="下载链接key" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="url" label="下载链接" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="priority" label="优先级" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column label="状态" header-align="center" align="center">
+        <template slot-scope="scope">
+          <el-switch style="display: block" v-model="scope.row.status" active-color="#13ce66" inactive-color="#ff4949" disabled></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column prop="appNum" label="应用号" header-align="center" align="center" :formatter="formatAppNum">
+      </el-table-column>
+      <el-table-column prop="remark" label="备注说明" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="createTime" label="创建时间" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="updateTime" label="修改时间" header-align="center" align="center">
+      </el-table-column>
+
+      <el-table-column label="操作" header-align="center" align="center">
+        <template slot-scope="scope">
+          <el-button icon="el-icon-edit" @click="editIosCompanySign(scope.row)" type="text" size="small">更新</el-button>
+          <el-button icon="el-icon-delete" @click="removeIosCompanySign(scope.row)" type="text" size="small" style="color: #F56C6C">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @current-change="handleCurrentChange"
+      @size-change="handleSizeChange"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      layout="->,total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+    <!--子组件-->
+    <add :ifshow="showAddFlag" @handleCloseDialog="showAddFlag=false;list();"></add>
+    <edit :ifshow="showEditFlag" :iosCompanySign="iosCompanySign" @handleCloseDialog="showEditFlag=false;list();"></edit>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchForm: {
+        osType: null
+      },
+      iosCompanySign: {},
+      tableData: [],
+      pageIndex: 1,
+      pageSize: 10,
+      total: 0,
+      selectIds: [],
+      showAddFlag: false,
+      showEditFlag: false
+    }
+  },
+  created () {
+    this.list()
+  },
+  methods: {
+    async list () {
+      let params = {
+        ...this.searchForm,
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      }
+      try {
+        const res = await this.$http.post('/ios-company-sign/list', params)
+        if (res.code === '200') {
+          this.tableData = res.data.rows
+          this.total = res.data.total
+        } else {
+          this.$message.error(res.message)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    handleCurrentChange (currentPage) {
+      this.pageIndex = currentPage
+      this.list()
+    },
+    handleSizeChange (size) {
+      this.pageSize = size
+      this.list()
+    },
+    handleSelectionChange (val) {
+      this.selectIds = []
+      val.forEach(v => {
+        this.selectIds.push(v.id)
+      })
+    },
+    editIosCompanySign (row) {
+      this.showEditFlag = true
+      this.iosCompanySign = row
+    },
+    removeIosCompanySign (row) {
+      let selectIdsStr = ''
+      let idsLength = this.selectIds.length
+      if (row instanceof Event) {
+        if (idsLength > 0) {
+          this.selectIds.forEach(v => {
+            selectIdsStr += `${v},`
+          })
+          selectIdsStr = selectIdsStr.substring(0, selectIdsStr.length - 1)
+        } else {
+          this.$message.warning('至少选择一条记录')
+          return
+        }
+      } else {
+        this.$refs.iosCompanySignTable.clearSelection()
+        idsLength = 1
+        this.selectIds.push(row.id)
+        selectIdsStr = row.id
+      }
+      const url = `/ios-company-sign?ids=${selectIdsStr}`
+      const tableLength = this.tableData.length
+      this.$confirm('确认删除吗？', '提示', {type: 'warning'}).then(async () => {
+        try {
+          const res = await this.$http.delete(url)
+          if (res.code === '200') {
+            this.$message.success('删除成功!')
+            // this.list()
+            this.selectIds.forEach(v => {
+              let i = this.tableData.findIndex(s => s.id === v)
+              this.tableData.splice(i, 1)
+            })
+            this.total -= idsLength
+            if (idsLength === tableLength) {
+              this.pageIndex = this.pageIndex > 1 ? this.pageIndex - 1 : 1
+              this.list()
+            }
+          } else {
+            this.$message.error(res.message)
+          }
+          this.selectIds = []
+        } catch (err) {
+          console.error(err)
+        }
+      }).catch(action => {
+        this.selectIds = []
+      })
+    }
+  },
+  components: {
+    'add': () => import('./add'),
+    'edit': () => import('./edit')
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
