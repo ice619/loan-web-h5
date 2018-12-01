6ef8229d0fda7a21561e8daad9c7ad0aa90cbff8
guozhengshui <guozhengshui@xiaoniu.com>
2018-12-01 21:20:14 +0800

    完善配置

--- config/test.env.js
+++ config/test.env.js
@@ -5,5 +5,5 @@ const devEnv = require('./dev.env')
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
   ENV_CONFIG: '"test"',
-  API_ROOT:'"//testfdd.51huihuahua.com"'
+  API_ROOT:'"//testfddweb.51huihuahua.com"'
 })
