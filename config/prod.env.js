2ec706bbf7838c1c1e517a38d84b527571baf709
giteadmin <gitea@xiaoniu.com>
2018-12-01 17:18:42 +0800

    更新 'config/prod.env.js'

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   API_ROOT:'"//fddapi.51huihuahua.com"',
-  WEB_ROOT:'"//fddweb.51huihuahua.com"'
+  WEB_ROOT:'"//fddapi.51huihuahua.com"'
 }
