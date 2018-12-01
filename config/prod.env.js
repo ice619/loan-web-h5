bf29d7f03e2f63de2e51392ea80ee26360128f7f
guozhengshui <guozhengshui@xiaoniu.com>
2018-12-01 18:55:52 +0800

    生产包V1.0.0

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  API_ROOT:'"//fddweb.51huihuahua.com"',
+  API_ROOT:'"//fddapi.51huihuahua.com"',
   WEB_ROOT:'"//fddweb.51huihuahua.com"'
 }
