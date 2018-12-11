735d179756663b59579a094092d523f1e1afc3c8
duanjun <D7prSrWpY2>
2018-12-11 15:28:41 +0800

    查询条件bug

--- src/views/customerReviewPushRiskLog/list.vue
+++ src/views/customerReviewPushRiskLog/list.vue
@@ -18,7 +18,7 @@
         <el-input v-model="searchForm.appSerialNumber" placeholder="申请单编号"/>
       </el-form-item>
       <el-form-item label="状态">
-        <el-select v-model="searchForm.pushStatus" clearable placeholder="请选择">
+        <el-select v-model="searchForm.status" clearable placeholder="请选择">
           <el-option v-for="item in $formatter.getSelectionOptions('pushStatus')" :key="item.value" :label="item.label" :value="item.value"/>
         </el-select>
       </el-form-item>
