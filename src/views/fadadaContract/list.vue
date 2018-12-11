405f59ea4291dc0ca4700f5bea0834b8845e7f51
licong <wo1355455Misyi>
2018-12-11 15:11:54 +0800

    测试打包是否正常

--- src/views/fadadaContract/list.vue
+++ src/views/fadadaContract/list.vue
@@ -28,6 +28,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" @click="list">查询</el-button>
+        <el-button type="primary" icon="el-icon-search" @click="list">搜索</el-button>
       </el-form-item>
     </el-form>
     <el-table ref="switchTable" :data="tableData" border stripe highlight-current-row @selection-change="handleSelectionChange">
