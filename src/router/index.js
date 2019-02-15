34a3f37248dcd7786fbfb4e96d430feeb10a2c58
guozhengshui <guozhengshui@xiaoniu.com>
2019-02-15 18:41:35 +0800

    新增功能:用户信息查询->查询用户登录日志

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,7 @@ import customerInfo from '@/views/customerInfo/base'
 import customerLatestReviewInfo from '@/views/customerLatestReviewInfo/list'
 import customerThirdPartyCertification from '@/views/customerThirdPartyCertification/list'
 import customerExt from '@/views/customerInfo/ext-list'
+import customerLoginLog from '@/views/customerLoginLog/list'
 Vue.use(Router)
 
 const router = new Router({
@@ -154,6 +155,11 @@ const router = new Router({
       path: '/customer-ext',
       name: 'customerExt',
       component: customerExt
+    },
+    {
+      path: '/customer-login-log',
+      name: 'customerLoginLog',
+      component: customerLoginLog
     }
   ]
 })
