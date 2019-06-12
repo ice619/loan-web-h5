c5e3689bee77831f0e053f4fd2cbdad4f8dad1b8
zhulinsen <zhulinsen@xiaoniu.com>
2019-06-12 20:15:33 +0800

    banner优化

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   // API_ROOT:'"//devfdd.51huihuahua.com"'
-  API_ROOT:'"//127.0.0.1:9803/loanweb"'
-  // API_ROOT:'"//testfqt.fqt188.com"'
+  // API_ROOT:'"//127.0.0.1:9803/loanweb"'
+  API_ROOT:'"//testfqt.fqt188.com"'
 })
