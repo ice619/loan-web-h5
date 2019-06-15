74cf99fd0e09225964c9e4532db1dae3bf18c018
zhulinsen <zhulinsen@xiaoniu.com>
2019-06-15 17:50:10 +0800

    修改测试环境地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  // API_ROOT:'"//devfdd.51huihuahua.com"'
+  API_ROOT:'"//devfdd.51huihuahua.com"'
   // API_ROOT:'"//127.0.0.1:9803/loanweb"'
-  API_ROOT:'"testloanwebh5.fqt188.com"'
+  // API_ROOT:'"testloanwebh5.fqt188.com"'
 })
