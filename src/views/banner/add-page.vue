c08b60dd0bb9ea28c4f174db7e06cace2af080ba
zhulinsen <zhulinsen@xiaoniu.com>
2019-06-18 22:12:11 +0800

    修改状态字段

--- src/views/banner/add-page.vue
+++ src/views/banner/add-page.vue
@@ -90,8 +90,8 @@
         <el-col :span="40">
           <el-form-item label="状态">
             <el-radio-group v-model="bannerForm.status">
-              <el-radio label="1">有效</el-radio>
-              <el-radio label="0">无效</el-radio>
+              <el-radio :label="1">有效</el-radio>
+              <el-radio :label="0">无效</el-radio>
             </el-radio-group>
           </el-form-item>
         </el-col>
