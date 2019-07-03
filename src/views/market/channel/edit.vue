3e1ec13e23d6b0dfa8fc41e386c79bd5ef548652
zhulinsen <zhulinsen@xiaoniu.com>
2019-07-03 17:28:45 +0800

    市场配置

---
+++ src/views/market/channel/edit.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="border">
+    <el-dialog title="编辑市场渠道" :visible.sync="ifshow" @open="openDialog" :before-close="closeDialog" width="45%">
+      <el-form :inline="true" :model="entryForm" :rules="rules" ref="entryForm" label-width="150px" class="demo-form-inline">
+        <el-row type="flex" justify="left">
+          <el-col :span="30">
+            <el-form-item label="投放渠道" prop="remark">
+              <el-input v-model="entryForm.channel" clearable style="width: 100%"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="left">
+          <el-col :span="30">
+            <el-form-item label="备注" prop="remark">
+              <el-input type="textarea" v-model="entryForm.remark" rows="4" placeholder="请输入备注" clearable style="width: 350px"/>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="center" style="margin-top: 30px">
+          <el-col :span="40">
+            <el-form-item>
+              <el-button style="color: white;background-color: #009688;" @click="save">提交</el-button>
+              <el-button @click="closeDialog">取消</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import debounce from 'throttle-debounce/debounce'
+import {clone} from '@/utils/common'
+import {getToken, getLanguage} from '@/utils/VueCookies'
+
+export default {
+  props: {
+    'ifshow': Boolean,
+    'entry': Object
+  },
+  data () {
+    return {
+      entryFormInitForm: {
+      },
+      entryForm: {},
+      rules: {},
+      activityUrl: `${process.env.API_ROOT}/upload-image-file`,
+      headers: {
+        'xxl_sso_sessionid': getToken(),
+        'language': getLanguage()
+      }
+    }
+  },
+  methods: {
+    openDialog () {
+      this.entryForm = clone(this.entry)
+    },
+    closeDialog () {
+      this.$refs['entryForm'].resetFields()
+      this.$emit('handleCloseDialog')
+    },
+    handleFilesChange: function (file, fileList) {
+      if (fileList.length > 1) {
+        fileList.shift()
+      }
+      if (file.status === 'ready') {
+        return
+      }
+      if (file.status === 'success' && file.response.code === '200') {
+        this.entryForm.imageUrl = file.response.data
+        this.$message.success('图片上传成功')
+      } else {
+        this.$message.error('图片上传失败')
+      }
+    },
+    save: debounce(300, function () {
+      this.$refs['entryForm'].validate(async (valid) => {
+        if (valid) {
+          try {
+            const res = await this.$http.put('/market-channel', this.entryForm)
+            if (res.code === '200') {
+              this.$message.success('修改成功!')
+              this.closeDialog()
+            } else {
+              this.$message.error(res.message)
+            }
+          } catch (err) {
+            console.error(err)
+          }
+        }
+      })
+    })
+  }
+}
+</script>
+
+<style lang="stylus">
+  .tip-info{
+    position: absolute;
+    margin-top: -10px;
+    font-size: 10px;
+    color: gray;
+    right: 0;
+  }
+  .sortable-ghost{
+    opacity: .8;
+    color: #fff!important;
+    background: #42b983!important;
+  }
+  .drag-handler{
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+  }
+  .avatar-uploader {
+    position: absolute;
+    top: 0px;
+    right: -123px;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
---
+++ src/views/market/detail/list.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="border">
+    <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+      <el-form-item label="APP名称">
+        <el-select v-model="searchForm.appName" clearable placeholder="请选择">
+          <el-option v-for="item in $formatter.getSelectionOptions('appName')" :key="item.value" :label="item.label" :value="item.value"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="投放渠道">
+        <el-select v-model="searchForm.channel" clearable placeholder="请选择">
+          <el-option v-for="item in channelOption" :key="item" :label="item" :value="item"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button style="color: white;background-color: #409eff;" icon="el-icon-search" @click="list">查询</el-button>
+        <el-button style="color: white;background-color: #409eff;" icon="el-icon-plus"  @click="showAddFlag = true">新增</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table ref="iosCompanySignTable" :data="tableData" border stripe highlight-current-row @selection-change="handleSelectionChange">
+      <el-table-column type="index" label="序号" width="50" header-align="center" align="center" />
+      <el-table-column prop="appName" label="APP名称" header-align="center" align="center">
+        <template slot-scope="scope">
+          <span>{{$formatter.simpleFormatSelection('appName', scope.row.appName)}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="channel" label="投放渠道" header-align="center" align="center" min-width="80"/>
+      <el-table-column prop="channelName" label="投放名称" header-align="center" align="center" min-width="80"/>
+      <el-table-column prop="channelFullUrl" label="投放链接" header-align="center" align="center" min-width="80"/>
+      <el-table-column prop="remark" label="备注" header-align="center" align="center" width="150" show-overflow-tooltip/>
+      <el-table-column prop="createTime" label="创建时间" header-align="center" align="center" min-width="80"/>
+      <el-table-column prop="updateUser" label="修改人" header-align="center" align="center" min-width="80"/>
+      <el-table-column prop="updateTime" label="修改时间" header-align="center" align="center" min-width="80"/>
+      <el-table-column label="操作" header-align="center" align="center">
+        <template slot-scope="scope">
+          <el-button icon="el-icon-edit" @click="editVariable(scope.row)" type="text" size="small" >编辑</el-button>
+          <el-button icon="el-icon-delete" @click="removeBanner(scope.row)" type="text" size="small" style="color: #F56C6C">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @current-change="handleCurrentChange"
+      @size-change="handleSizeChange"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      layout="->,total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+    <!--子组件-->
+    <add :ifshow="showAddFlag" @handleCloseDialog="showAddFlag=false;list();"></add>
+    <edit :ifshow="showEditFlag" :entry="entry" @handleCloseDialog="showEditFlag=false;list();"></edit>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchForm: {
+        appName: 21,
+        channel: null
+      },
+      entry: {},
+      tableData: [],
+      pageIndex: 1,
+      pageSize: 10,
+      total: 0,
+      selectIds: [],
+      showAddFlag: false,
+      showEditFlag: false,
+      channelOption: []
+    }
+  },
+  created () {
+    this.list()
+    this.getChannelOption()
+  },
+  methods: {
+    async getChannelOption () {
+      let params = {
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      }
+      try {
+        const res = await this.$http.post('/market-channel/page', params)
+        if (res.code === '200') {
+          let rows = res.data.rows
+          this.channelOption = []
+          rows.forEach(item => {
+            this.channelOption.push(item.channel)
+          })
+        } else {
+          this.$message.error(res.message)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    async list () {
+      let params = {
+        ...this.searchForm,
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize
+      }
+      try {
+        const res = await this.$http.post('/market-channel-detail/page', params)
+        if (res.code === '200') {
+          this.tableData = res.data.rows
+          this.total = res.data.total
+        } else {
+          this.$message.error(res.message)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    handleCurrentChange (currentPage) {
+      this.pageIndex = currentPage
+      this.list()
+    },
+    handleSizeChange (size) {
+      this.pageSize = size
+      this.list()
+    },
+    handleSelectionChange (val) {
+      this.selectIds = []
+      val.forEach(v => {
+        this.selectIds.push(v.id)
+      })
+    },
+    editVariable (row) {
+      this.showEditFlag = true
+      this.entry = row
+    },
+    async removeBanner (row) {
+      let selectIdsStr = ''
+      let idsLength = this.selectIds.length
+      if (row instanceof Event) {
+        if (idsLength > 0) {
+          this.selectIds.forEach(v => {
+            selectIdsStr += `${v},`
+          })
+          selectIdsStr = selectIdsStr.substring(0, selectIdsStr.length - 1)
+        } else {
+          this.$message.warning('至少选择一条记录')
+          return
+        }
+      } else {
+        this.$refs.iosCompanySignTable.clearSelection()
+        idsLength = 1
+        this.selectIds.push(row.id)
+        selectIdsStr = row.id
+      }
+      const tableLength = this.tableData.length
+      this.$confirm('确认删除吗？', '提示', {type: 'warning'}).then(async () => {
+        try {
+          const res = await this.$http.delete(`/market-channel-detail/${selectIdsStr}`)
+          if (res.code === '200') {
+            this.$message.success('删除成功!')
+            // this.list()
+            this.selectIds.forEach(v => {
+              let i = this.tableData.findIndex(s => s.id === v)
+              this.tableData.splice(i, 1)
+            })
+            this.total -= idsLength
+            if (idsLength === tableLength) {
+              this.pageIndex = this.pageIndex > 1 ? this.pageIndex - 1 : 1
+              this.list()
+            }
+          } else {
+            this.$message.error(res.message)
+          }
+          this.selectIds = []
+        } catch (err) {
+          console.error(err)
+        }
+      }).catch(action => {
+        this.selectIds = []
+      })
+    }
+  },
+  components: {
+    'add': () => import('./add'),
+    'edit': () => import('./edit')
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
