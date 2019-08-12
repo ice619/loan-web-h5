a1410941b0bd99d69f04cffb3f7eb01ea5c606b6
zhangshuji <zhangshuji@xiaoniu.com>
2019-08-12 16:45:45 +0800

    开发环境切换api ip

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,7 +7,7 @@ module.exports = merge(prodEnv, {
   ENV_CONFIG: '"dev"',
   LOGIN_URL: '"http://devxiaoniuwebui.58fqt.com/login"',
   // LOGIN_URL: '"https://testxiaoniuwebui.fqt188.com/login"',
-  API_ROOT:'"http://127.0.0.1:9015/loanweb"'
-  // API_ROOT:'"http://172.16.11.40:9015/loanweb"'
+  // API_ROOT:'"http://127.0.0.1:9015/loanweb"'
+  API_ROOT:'"http://172.16.11.40:9015/loanweb"'
   // API_ROOT:'"https://testcallback.fqt188.com/loanweb"',
 })
