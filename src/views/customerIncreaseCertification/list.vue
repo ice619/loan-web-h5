381cd73a6be6f8282b379fa058013eb1b98c8f43
zhangshuji <zhangshuji@xiaoniu.com>
2019-08-13 14:32:31 +0800

    提额认证查询

--- src/views/customerIncreaseCertification/list.vue
+++ src/views/customerIncreaseCertification/list.vue
@@ -3,30 +3,29 @@
     <el-form :inline="true" :model="searchForm" class="demo-form-inline">
       <el-form-item label="APP名称：">
         <el-select v-model="searchForm.appName" clearable placeholder="请选择APP名称">
-          <el-option v-for="item in $formatter.getSelectionOptions('appNames')" :key="item.value" :label="item.label" :value="item.value"/>
+          <el-option v-for="item in $formatter.getSelectionOptions('appName')" :key="item.value" :label="item.label" :value="item.value"/>
         </el-select>
       </el-form-item>
       <el-form-item label="客户编号：">
         <el-input v-model="searchForm.customerId" clearable placeholder="请输入客户编号" style="width: 300px"></el-input>
       </el-form-item>
+      <el-form-item label="认证时间">
+        <el-date-picker v-model="searchForm.startCertificationTime" type="datetime" placeholder="选择开始时间" value-format="yyyy-MM-dd HH:mm:ss"/>
+        ~
+        <el-date-picker v-model="searchForm.endCertificationTime" type="datetime" placeholder="选择结束时间" value-format="yyyy-MM-dd HH:mm:ss"/>
+      </el-form-item>
       <el-form-item>
-        <el-button style="color: white;background-color: #009688;" type="primary" icon="el-icon-search" @click="pageIndex=1;list();">搜索</el-button>
-        <el-button @click="back" v-show="showBackFlag">返回</el-button>
+        <el-button style="color: white;background-color: #009688;" type="primary" icon="el-icon-search" @click="pageIndex=1;list()">搜索</el-button>
       </el-form-item>
     </el-form>
-    <el-table ref="customerThirdPartyCertificationTable" :data="tableData" border stripe highlight-current-row
+    <el-table ref="customerIncreaseCertificationTable" :data="tableData" border stripe highlight-current-row
               @selection-change="handleSelectionChange">
       <el-table-column prop="appName" label="APP名称" header-align="center" align="center">
         <template slot-scope="scope">
-          <span>{{$formatter.simpleFormatSelection('appNames', scope.row.appName)}}</span>
+          <span>{{$formatter.simpleFormatSelection('appName', scope.row.appName)}}</span>
         </template>
       </el-table-column>
       <el-table-column prop="customerId" label="客户编号" header-align="center" align="center" min-width="285">
-      </el-table-column>
-      <el-table-column prop="businessType" label="业务类型" header-align="center" align="center">
-        <template slot-scope="scope">
-          <span>{{$formatter.simpleFormatSelection('businessType', scope.row.businessType)}}</span>
-        </template>
       </el-table-column>
       <el-table-column prop="certificationType" label="认证类型" header-align="center" align="center" min-width="95">
         <template slot-scope="scope">
@@ -40,7 +39,7 @@
       </el-table-column>
       <el-table-column prop="certificationTime" label="认证时间" header-align="center" align="center" min-width="160"/>
       <el-table-column prop="createTime" label="创建时间" header-align="center" align="center" min-width="160"/>
-      <el-table-column prop="modifyTime" label="修改时间" header-align="center" align="center" min-width="160"/>
+      <el-table-column prop="updateTime" label="修改时间" header-align="center" align="center" min-width="160"/>
     </el-table>
   </div>
 </template>
@@ -51,37 +50,28 @@ export default {
   data () {
     return {
       searchForm: {
-        appName: 7,
-        customerId: null
+        appName: 21,
+        customerId: null,
+        startCertificationTime: null,
+        endCertificationTime: null
       },
-      customerThirdPartyCertificationWindow: {},
       tableData: [],
       pageIndex: 1,
       pageSize: 10,
       total: 0,
       selectIds: [],
-      showBackFlag: false,
       phone: null
     }
   },
   created () {
-    this.initList()
+    this.initSearchForm()
+    this.list()
   },
   methods: {
-    back () {
-      this.$router.push({name: 'customerInfo', params: {appName: this.$route.params.appName, phone: this.phone, customerId: this.$route.params.customerId}})
-    },
-    async initList () {
-      let appName = this.$route.params.appName
-      let customerId = this.$route.params.customerId
-      let phone = this.$route.params.phone
-      if (appName && customerId) {
-        this.searchForm.appName = appName
-        this.searchForm.customerId = customerId
-        this.phone = phone
-        this.showBackFlag = true
-        this.list()
-      }
+    initSearchForm () {
+      const now = new Date()
+      this.searchForm.startCertificationTime = `${this.formatDate(now, 'yyyy-MM-dd')} 00:00:00`
+      this.searchForm.endCertificationTime = `${this.formatDate(now, 'yyyy-MM-dd')} 23:59:59`
     },
     async list () {
       let params = {
@@ -90,19 +80,9 @@ export default {
         pageSize: this.pageSize
       }
       try {
-        let appName = this.searchForm.appName
-        if (!appName) {
-          this.$message.error('APP名称不能为空')
-          return
-        }
-        let customerId = this.searchForm.customerId
-        if (!customerId) {
-          this.$message.error('用户编号不能为空')
-          return
-        }
-        const res = await this.$http.post('/management/customer/third-party-certification-info', params)
+        const res = await this.$http.post('/customer/increase-certification-info', params)
         if (res.code === '200') {
-          this.tableData = res.data
+          this.tableData = res.data.rows
         } else {
           this.$message.error(res.message)
         }
