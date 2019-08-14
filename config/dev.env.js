5bc344465ab4782be1182ab8f197910071a2eebb
duanjun <D7prSrWpY2>
2019-08-14 15:10:35 +0800

    行为奖励配置编辑优化

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,9 +5,9 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  LOGIN_URL: '"http://devxiaoniuwebui.58fqt.com/login"',
-  // LOGIN_URL: '"https://testxiaoniuwebui.fqt188.com/login"',
+  // LOGIN_URL: '"http://devxiaoniuwebui.58fqt.com/login"',
+  LOGIN_URL: '"https://testxiaoniuwebui.fqt188.com/login"',
   // API_ROOT:'"http://127.0.0.1:9015/loanweb"'
-  API_ROOT:'"http://172.16.11.40:9015/loanweb"'
-  // API_ROOT:'"https://testcallback.fqt188.com/loanweb"',
+  // API_ROOT:'"http://172.16.11.40:9015/loanweb"'
+  API_ROOT:'"https://testcallback.fqt188.com/loanweb"',
 })
